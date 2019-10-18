2c032497bd97e18d60ed03b5d37f17a4803795eb
Landon Mitchell <landonjmitchell@cs.uchicago.edu>
2019-10-18 14:47:42 -0700

    Re-ordered test graphs

--- graphs/test_graphs.py
+++ graphs/test_graphs.py
@@ -1,7 +1,35 @@
 from graphs import Graph
 
+
 """
 Graph A:
+Undirected, Acyclic, Weighted, Disconnected, Bipartite
+
+    /---(4)---B---(8)---C---(7)---D
+   /          |           \
+  /         (11)           \--(4)--F--(10)--E
+ /            |
+A             |
+              |  /--(7)--I
+              | /
+              H---(1)----G
+
+Y---(5)---Z
+"""
+
+graph_a = Graph()
+graph_a.add_edge('A', 'B', 4)
+graph_a.add_edge('B', 'C', 8)
+graph_a.add_edge('B', 'H', 11)
+graph_a.add_edge('C', 'D', 7)
+graph_a.add_edge('C', 'F', 4)
+graph_a.add_edge('E', 'F', 10)
+graph_a.add_edge('G', 'H', 1)
+graph_a.add_edge('H', 'I', 7)
+graph_a.add_edge('Y', 'Z', 5)
+
+"""
+Graph B:
 Undirected, Cyclic, Weighted, Disconnected, Not Bipartite
 
     /---(4)---B---(8)---C---(7)---D---(9)---\
@@ -17,56 +45,25 @@ A        (11)~|   (7)---I   (4)   |~(14)        E
 Y---(5)---Z
 """
 
-graph_a = Graph()
-graph_a.add_edge('A', 'B', 4)
-graph_a.add_edge('A', 'H', 8)
-graph_a.add_edge('B', 'C', 8)
-graph_a.add_edge('B', 'H', 11)
-graph_a.add_edge('C', 'D', 7)
-graph_a.add_edge('C', 'F', 4)
-graph_a.add_edge('C', 'I', 2)
-graph_a.add_edge('D', 'E', 9)
-graph_a.add_edge('D', 'F', 14)
-graph_a.add_edge('E', 'F', 10)
-graph_a.add_edge('F', 'G', 2)
-graph_a.add_edge('G', 'H', 1)
-graph_a.add_edge('G', 'I', 6)
-graph_a.add_edge('H', 'I', 7)
-graph_a.add_edge('Y', 'Z', 5)
-
-
-"""
-
-Graph B:
-Undirected, Acyclic, Weighted, Disconnected, Bipartite
-
-    /---(4)---B---(8)---C---(7)---D
-   /          |           \
-  /         (11)           \--(4)--F--(10)--E
- /            |
-A             |
-              |  /--(7)--I
-              | /
-              H---(1)----G
-
-Y---(5)---Z
-
-"""
-
 graph_b = Graph()
 graph_b.add_edge('A', 'B', 4)
+graph_b.add_edge('A', 'H', 8)
 graph_b.add_edge('B', 'C', 8)
 graph_b.add_edge('B', 'H', 11)
 graph_b.add_edge('C', 'D', 7)
 graph_b.add_edge('C', 'F', 4)
+graph_b.add_edge('C', 'I', 2)
+graph_b.add_edge('D', 'E', 9)
+graph_b.add_edge('D', 'F', 14)
 graph_b.add_edge('E', 'F', 10)
+graph_b.add_edge('F', 'G', 2)
 graph_b.add_edge('G', 'H', 1)
+graph_b.add_edge('G', 'I', 6)
 graph_b.add_edge('H', 'I', 7)
 graph_b.add_edge('Y', 'Z', 5)
 
 
 """
-
 Graph C:
 Directed, Acyclic, Weighted, Disconnected, Bipartite
 
@@ -80,7 +77,6 @@ A             v                    /---(12)>>---/
               H------<<(1)------G
 
 Y--(5)>>--Z
-
 """
 
 graph_c = Graph(directed=True)
@@ -97,7 +93,6 @@ graph_c.add_edge('Y', 'Z', 5)
 
 
 """
-
 Graph D:
 Directed, Cyclic, Weighted, Disconnected, Not Bipartite
 
