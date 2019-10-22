a0cc996106b93d362739eb72b83c3232890fb588
Landon Mitchell <landonjmitchell@cs.uchicago.edu>
2019-10-22 13:36:55 -0700

    added bfs bipartite check

--- graphs/graph_properties.py
+++ graphs/graph_properties.py
@@ -36,3 +36,32 @@ def has_undirected_cycle(graph):
                 color[vertex] = "black" if temp_color[vertex] == "black" else color[vertex]
 
     return has_cycle
+
+
+def is_bipartite(graph):
+    """ Determines if a graph is bipartite.
+
+        Determine if a graph is bipartite using breadth first search. Tests
+        all connected components of a disconnected graph.
+
+        Parameters
+        ----------
+        graph : Graph instance
+            The graph to test as bipartite
+
+        Returns
+        -------
+        distance : bool
+            Indication of whether the graph is bipartite
+    """
+
+    color, d, p, hc, is_bipartite = bfs.bfs(graph, next(iter(graph.vertices)))
+    for vertex in graph.vertices:
+        if not is_bipartite:
+            return False
+        if color[vertex] == "white":
+            temp_color, d, p, hc, is_bipartite = bfs.bfs(graph, vertex)
+            for vertex in graph.vertices:
+                color[vertex] = "black" if temp_color[vertex] == "black" else color[vertex]
+
+    return is_bipartite
