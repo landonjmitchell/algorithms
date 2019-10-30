559b1d63aad9e47d0ecca0c9b5b905b0fb649ee2
Landon Mitchell <landonjmitchell@cs.uchicago.edu>
2019-10-30 11:45:33 -0700

    Added binary tree exceptions

---
+++ binary_trees/exceptions.py
@@ -0,0 +1,24 @@
+class MissingNodeError(KeyError):
+    """ Error indicating a node at a specific tree position does not exist.
+    """
+
+    def __init__(self, depth, position, *args, **kwargs):
+        msg = f"No node exists at depth: {depth}, position {position}"
+        super().__init__(msg, *args, **kwargs)
+
+
+class NodePositionError(IndexError):
+    """ Error indicating the given binary tree position does not exist.
+    """
+
+    def __init__(self, depth, position, *args, **kwargs):
+        msg = f"Invalid tree position: (depth: {depth}, position {position})"
+        super().__init__(msg, *args, **kwargs)
+
+
+class NodePositionOccupiedError(Exception):
+    """ Error indication a node position is already occupied.
+    """
+
+    pass
+
