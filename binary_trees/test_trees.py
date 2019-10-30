8bf86e66cda6e6690ba940e49ff5d8efa27d2cb2
Landon Mitchell <landonjmitchell@cs.uchicago.edu>
2019-10-30 11:44:18 -0700

    Added binary tree tests and test trees

---
+++ binary_trees/test_trees.py
@@ -0,0 +1,16 @@
+from binary_tree import BinaryTree, Node
+
+r"""
+
+Tree A:
+
+   5
+  / \
+ 3   9
+
+"""
+
+tree_a = BinaryTree()
+tree_a.add_node(Node(5), 0, 0)
+tree_a.add_node(Node(3), 1, 0)
+tree_a.add_node(Node(8), 1, 1)
---
+++ binary_trees/tests.py
@@ -0,0 +1,78 @@
+import unittest
+
+from test_trees import (tree_a)
+from binary_tree import BinaryTree, Node
+from exceptions import MissingNodeError, NodePositionOccupiedError, NodePositionError
+
+class TestBinaryTreeFunctions(unittest.TestCase):
+
+    def test_add_get_node(self):
+        """ Tests adding and retrieving nodes at specific positions.
+        """
+
+        self.test_node_1 = Node(99)
+        tree_a.add_node(self.test_node_1, 2, 0)
+        node = tree_a.get_node(2, 0)
+        self.assertTrue(node is self.test_node_1)
+
+        with self.assertRaises(MissingNodeError):
+            tree_a.get_node(2, 1)
+            tree_a.add_node(Node(77), 5, 1)
+
+        with self.assertRaises(NodePositionError):
+            tree_a.get_node(-1, 0)
+            tree_a.add_node(Node(66), 2, 3)
+            tree_a.add_node(Node(55), 4, -1)
+
+    def test_value_at_node(self):
+        """ Tests returning the value of a node at a specific position.
+        """
+
+        self.assertTrue(tree_a.value_at_node(0, 0), 5)
+        self.assertTrue(tree_a.value_at_node(1, 0), 3)
+        self.assertTrue(tree_a.value_at_node(1, 1), 8)
+        self.assertTrue(tree_a.value_at_node(2, 0), 99)
+
+        with self.assertRaises(MissingNodeError):
+            tree_a.value_at_node(3, 0)
+            tree_a.value_at_node(2, 1)
+
+        with self.assertRaises(NodePositionError):
+            tree_a.value_at_node(-1, 0)
+            tree_a.value_at_node(2, 3)
+            tree_a.value_at_node(4, -1)
+
+    def test_is_balanced(self):
+        """ Tests determining if binary tree is balanced.
+        """
+        pass
+
+    def test_is_bst(self):
+        """ Tests determining if binary tree is a binary search tree.
+        """
+        pass
+    
+class TestNodeFunctions(unittest.TestCase):
+
+    def test_add_children(self):
+        """ Tests adding children to a node.
+        """
+
+        parent_node = tree_a.get_node(2, 0)
+        new_node_left = Node('left')
+        new_node_right = Node('right')
+
+        parent_node.add_left_child(new_node_left)
+        child_node_left = tree_a.get_node(3, 0)
+        self.assertTrue(new_node_left is child_node_left)
+
+        parent_node.add_right_child(new_node_right)
+        child_node_right = tree_a.get_node(3, 1)
+        self.assertTrue(new_node_right is child_node_right)
+
+        with self.assertRaises(NodePositionOccupiedError):
+            parent_node.add_left_child(new_node_left)
+            parent_node.add_right_child(new_node_right)
+
+if __name__ == "__main__":
+    unittest.main()
